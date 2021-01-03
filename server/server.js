fbba969b9b928b224ea34734168e54803a3d3ffa
Jaydon Goodrich <jaydongoodrich@gmail.com>
2021-01-03 00:03:50 -0700

    JST

--- server/server.js
+++ server/server.js
@@ -1,16 +1,20 @@
 const express = require('express');
 
 const { ApolloServer } = require('apollo-server-express');
+const { authMiddleware } = require('./utils/auth');
+
 
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
+const auth = require('./utils/auth');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+  context: authMiddleware
 });
 
 server.applyMiddleware({ app });
